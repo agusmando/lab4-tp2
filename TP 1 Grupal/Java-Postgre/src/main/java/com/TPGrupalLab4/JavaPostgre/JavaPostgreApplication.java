0ff365151c55cce53ff1ed471587eb58dcfcd86d
GabrielGiorgis <gabi.giorgis6@gmail.com>
2024-04-03 11:23:55 -0300

    Inicialización del proyecto

---
+++ TP 1 Grupal/Java-Postgre/src/main/java/com/TPGrupalLab4/JavaPostgre/JavaPostgreApplication.java
@@ -0,0 +1,13 @@
+package com.TPGrupalLab4.JavaPostgre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaPostgreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaPostgreApplication.class, args);
+	}
+
+}
---
+++ TP 1 Grupal/Java-Postgre/src/test/java/com/TPGrupalLab4/JavaPostgre/JavaPostgreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TPGrupalLab4.JavaPostgre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaPostgreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
