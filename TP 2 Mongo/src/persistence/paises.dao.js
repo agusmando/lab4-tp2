e2266c2c9cc97a5394263913c9aa8d6e36b73dcb
MIsabelJ <maria.jurman@alumnos.frm.utn.edu.ar>
2024-04-04 11:40:34 -0300

    Paises

--- TP 2 Mongo/src/persistence/paises.dao.js
+++ TP 2 Mongo/src/persistence/paises.dao.js
@@ -1,1 +1,56 @@
-//llamar al modelo de pais, hacer el guardado u
+const { connectMongo } = require("../data/config/mongodb.config");
+const Pais = require("../data/model/pais.model");
+
+class PaisesDAO {
+    constructor() {
+        connectMongo();
+    }
+
+    async getAll() {
+        try {
+            const paises = await Pais.find();
+            return paises;
+        } catch (error) {
+            console.log("error: " + error);
+        }
+    }
+
+    async getOne(id) {
+        try {
+            const paises = await Pais.findById(id);
+            return paises;
+        } catch (error) {
+            console.log("error: " + error);
+        }
+    }
+
+    async create(data) {
+        try {
+            const paises = new Pais(data);
+            await paises.save();
+            return paises;
+        } catch (error) {
+            console.log("error: " + error);
+        }
+    }
+
+    async update(id, data) {
+        try {
+            const paises = await Pais.findByIdAndUpdate(id, data);
+            return paises;
+        } catch (error) {
+            console.log("error: " + error);
+        }
+    }
+
+    async delete(id) {
+        try {
+            const paises = await Pais.findByIdAndDelete(id);
+            return paises;
+        } catch (error) {
+            console.log("error: " + error);
+        }
+    }
+}
+
+module.exports = PaisesDAO;
