b591d31f4054054d9a0cbd3e57acfb644cc777b6
MIsabelJ <maria.jurman@alumnos.frm.utn.edu.ar>
2024-04-04 12:02:14 -0300

    Cambios service

--- TP 2 Mongo/src/service/paises.service.js
+++ TP 2 Mongo/src/service/paises.service.js
@@ -1,5 +1,14 @@
-const daoPaises = require("../persistence/paises.dao.js");
+const DaoPaises = require("../persistence/paises.dao.js");
 
-const getAll = async() => {};
+const daoPais = new DaoPaises();
+
+const getAll = async() => {
+    try {
+        const paises = await daoPais.getAll();
+        return paises;
+    } catch (error) {
+        console.log("error: " + error);
+    }
+};
 
 const populate = async() => {};
